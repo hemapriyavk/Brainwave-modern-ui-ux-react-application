9632db2db07eba18557b9305e3569dba0c0d8646
hemapriyavk <hemapriya2000mdu@gmail.com>
2024-10-26 23:49:12 +0530

    Constants error cleared 26/10/24

--- src/Components/RoadMap.jsx
+++ src/Components/RoadMap.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 import Heading from "./Heading";
 import Section from "./Section";
 import TagLine from "./TagLine";
-import { roadmap } from "../constants";
+import { roadmap } from "../Constants";
 import { check2, grid, loading1 } from "../assets";
 import { Gradient } from "./design/Roadmap";
 
